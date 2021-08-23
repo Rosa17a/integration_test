3f9dac86bb7cee1abe1b35040db9779d6595e78d
Roza Shakhbazyan <roza.shakhbazyan@gmail.com>
2021-08-23 12:26:50 +0400

    Initial commit

---
+++ android/app/src/androidTest/java/com/example/integration_test/MainActivityText.java
@@ -0,0 +1,17 @@
+// Change this line to YOUR package name:
+package com.example.integration_test;
+
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+
+import org.graalvm.compiler.core.match.MatchRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+// Import MainActivity from YOUR package
+import com.example.integration_test.MainActivity;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
